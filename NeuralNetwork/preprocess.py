14ccf4e30f0aadcc5b0c07c274dc45d773257bb4
isomorphicdude <tw1320@ic.ac.uk>
2022-08-27 00:24:25 +0200

    add scaling option

--- NeuralNetwork/preprocess.py
+++ NeuralNetwork/preprocess.py
@@ -1,29 +1,44 @@
+from tokenize import Triple
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from sklearn.preprocessing import normalize, MinMaxScaler 
 
-def getDatasets(dataframe):
+# define a global variable minmax_scaler for later to reverse the transformation
+minmax_scaler = MinMaxScaler()
+
+
+def getDatasets(dataframe, scaling = True):
     """
-    Returns tuple of train, valid, and test datasets.
+    Returns tuple of scaled train, valid, and test datasets.  
+
+    Args:
+        - dataframe: ndarray, dataframe of the data  
+
+    Output:  
+        - (train_ds, valid_ds, test_ds): tuple of datasets which are between 0 and 1
+    
     """
     N = len(dataframe)
     indices = np.random.permutation(N)
     train, val, test = np.split(dataframe[indices], [int(.8*N), int(.9*N)])
+    # add normalizing layer here
+    if scaling:
+        train = minmax_scaler.fit_transform(train)
+        val = minmax_scaler.transform(val)
+        test = minmax_scaler.transform(test)
     print(train.shape, val.shape, test.shape)
     train_ds = tf.data.Dataset.from_tensor_slices(tf.cast(train, tf.float32))
     valid_ds = tf.data.Dataset.from_tensor_slices(tf.cast(val, tf.float32))
     test_ds  = tf.data.Dataset.from_tensor_slices(tf.cast(test, tf.float32))
-    # train_ds = 0
-    # valid_ds = 0
-    # test_ds  = 0
-
     return (train_ds, valid_ds, test_ds)
+
 
 def shuffle_and_batch_dataset(dataset, batch_size, shuffle_buffer=None):
     """
     Returns the shuffled and batched Dataset.  
 
-    Args:
+    Args:  
         - dataset: tf.data.Dataset
         - batch_size: int, batch size
         - shuffle_buffer: int, shuffle buffer size
@@ -60,7 +75,29 @@ def xy_split(data):
 
     return (data[:,:-1], data[:,-1])  
 
-#TODO: add pipeline for preprocessing
-def pipeline1():
-    pass
-    
+
+
+def pipeline1(dataframe_BS, prefetch = True, scaling = True):
+    """
+    Returns a tuple of processed prefetched data for training. 
+
+    Args:
+        - dataframe_BS: ndarray, dataframe of the data
+        - prefetch: boolean, whether to prefetch the data
+        - scaling: boolean, whether to scale the data
+    Output:
+        - (train_ds, valid_ds, test_ds): tuple of datasets
+    """  
+    train_ds, valid_ds, test_ds = getDatasets(dataframe_BS, scaling)
+    train_ds = shuffle_and_batch_dataset(train_ds, batch_size=32)
+    valid_ds = shuffle_and_batch_dataset(valid_ds, batch_size=32)
+    test_ds = shuffle_and_batch_dataset(test_ds, batch_size=32)
+    train_ds = map_dataset(train_ds, xy_split)
+    valid_ds = map_dataset(valid_ds, xy_split)
+    test_ds = map_dataset(test_ds, xy_split)
+    if prefetch:
+        train_ds = train_ds.prefetch(tf.data.experimental.AUTOTUNE)
+        valid_ds = valid_ds.prefetch(tf.data.experimental.AUTOTUNE)
+        test_ds = test_ds.prefetch(tf.data.experimental.AUTOTUNE)
+
+    return (train_ds, valid_ds, test_ds)
