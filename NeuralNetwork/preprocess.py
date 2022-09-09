6509e5024bb6ee6f39b850fe4ce30079a37a8b61
isomorphicdude <tw1320@ic.ac.uk>
2022-09-09 18:51:33 +0200

    add new function

--- NeuralNetwork/preprocess.py
+++ NeuralNetwork/preprocess.py
@@ -49,6 +49,28 @@ def shuffle_and_batch_dataset(dataset, batch_size, shuffle_buffer=None):
     
     return out  
 
+# def flattenDim(dim, inputs):
+#     """
+    
+#     Returns a pandas dataframe with assest price and rho expanded.  
+
+#     Args:  
+#         - dim: dimension of assest price basket
+#         - inputs: pandas dataframe of basket options, containing columns
+#                   'underlyings_price' and 'rho'  
+
+#     Output:  
+#         - outputs: pandas dataframe
+#     """
+#     for i in range(dim):
+#         inputs[f'Underlying_{i}'] = np.vstack(inputs.underlyings_price.values)[:,i]
+
+#     for i in range(dim):
+#         inputs[f'Rho_{i}'] = np.vstack(inputs.rho.values)[:,i]
+#     inputs = inputs.drop(['underlyings_price', 'rho'], axis=1)
+#     return inputs
+
+
 def map_dataset(dataset, map_func):
     """
     Return a mapped dataset.  
