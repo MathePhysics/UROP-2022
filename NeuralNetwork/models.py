448b7b455e7785617c469f1a4591b4b69b14af28
isomorphicdude <47920591+isomorphicdude@users.noreply.github.com>
2022-09-12 18:32:13 +0100

    add residual block and rename

--- NeuralNetwork/models.py
+++ NeuralNetwork/models.py
@@ -2,13 +2,16 @@ import argparse
 import numpy as np
 import tensorflow as tf
 
+from tensorflow.keras.layers import Dense, Input, Flatten, Dropout, BatchNormalization
+from tensorflow.keras.regularizers import l1, l2, l1_l2
+
 def getModel(input_shape = (7,),
             num_layers   = 2,
             hidden_units = [14,7],
             output_shape = (1,),
             activation = 'elu',
             initializer = tf.random_normal_initializer(mean=0.0, stddev=0.1),
-            regularizer = tf.keras.regularizers.l1_l2(0.000001,0.000001),
+            regularizer = l1_l2(0.000001,0.000001),
             final_activation = None,
             dropout = None,
             batchnorm = False
@@ -35,22 +38,22 @@ def getModel(input_shape = (7,),
     if dropout is not None:  
         assert num_layers == len(dropout), "Number of dropout rates must match number of layers"
 
-    inputs = tf.keras.layers.Input(shape=input_shape)
-    h = tf.keras.layers.Flatten()(inputs)
+    inputs = Input(shape=input_shape)
+    h = Flatten()(inputs)
 
     for i, layer in enumerate(hidden_units):
-        h = tf.keras.layers.Dense(layer, activation=activation, 
+        h = Dense(layer, activation=activation, 
                                   kernel_initializer = initializer,
                                   kernel_regularizer = regularizer)(h)
         if dropout:
-            h = tf.keras.layers.Dropout(dropout[i])(h)
+            h = Dropout(dropout[i])(h)
         if batchnorm:
-            h = tf.keras.layers.BatchNormalization()(h)
+            h = BatchNormalization()(h)
     if final_activation is not None:
-        outputs = tf.keras.layers.Dense(output_shape[0], activation=final_activation,
+        outputs = Dense(output_shape[0], activation=final_activation,
                                         kernel_initializer = initializer)(h)
     else:
-        outputs = tf.keras.layers.Dense(output_shape[0], 
+        outputs = Dense(output_shape[0], 
                                         kernel_initializer = initializer)(h)
 
     model = tf.keras.Model(inputs=inputs, outputs=outputs)  
@@ -102,21 +105,21 @@ def model_builder_basket(dim,
     dim_glob = dim
     output_shape_glob = output_shape
 
-    inputs = tf.keras.layers.Input(shape=input_shape)
-    h = tf.keras.layers.Flatten()(inputs)
+    inputs = Input(shape=input_shape)
+    h = Flatten()(inputs)
 
     for i, layer in enumerate(hidden_units):
-        h = tf.keras.layers.Dense(layer, activation=activation, 
+        h = Dense(layer, activation=activation, 
                                   kernel_initializer = initializer)(h)
         if dropout:
-            h = tf.keras.layers.Dropout(dropout[i])(h)
+            h = Dropout(dropout[i])(h)
         if batchnorm:
-            h = tf.keras.layers.BatchNormalization()(h)
+            h = BatchNormalization()(h)
     if final_activation is not None:
-        outputs = tf.keras.layers.Dense(output_shape[0], activation=final_activation,
+        outputs = Dense(output_shape[0], activation=final_activation,
                                         kernel_initializer = initializer)(h)
     else:
-        outputs = tf.keras.layers.Dense(output_shape[0], 
+        outputs = Dense(output_shape[0], 
                                         kernel_initializer = initializer)(h)
 
     model = tf.keras.Model(inputs=inputs, outputs=outputs)  
@@ -272,3 +275,47 @@ def tuned_model(hp):
                 metrics = [tf.keras.metrics.MeanSquaredError()])
 
     return model
+
+class DenseResidualBlock(tf.keras.layers.Layer):
+    
+    def __init__(self, layer_per_block=[10,10,10], activation='elu', 
+                initializer='random_uniform',
+                l2reg_coeff=0.01, **kwargs):
+        """
+        Class initializer for a custom dense residual block with batch normalization.  
+
+        Args:   
+            - layer_per_block: number of layers in the block
+            - activation: string, activation function to use in the dense layers
+            - initializer: tf.keras.initializers, initializer to use in the dense layer
+            - l2reg_coeff: coefficient for L2 regularization
+            - **kwargs: keyword arguments for the parent class  
+        """  
+
+        super(DenseResidualBlock, self).__init__(**kwargs)
+
+        self.l2reg_coeff = l2reg_coeff
+        self.activation = activation
+        self.layer_per_block = layer_per_block
+        self.initializer = initializer
+
+
+    def build(self, input_shape):
+        for i in range(len(self.layer_per_block)):
+            setattr(self, 'dense_{}'.format(i+1), Dense(self.layer_per_block[i],
+                                                    activation=self.activation,
+                                                    kernel_initializer=self.initializer,
+                                                    kernel_regularizer=l2(self.l2reg_coeff),
+                                                    input_shape = input_shape,
+                                                    name='dense_{}'.format(i+1)))
+                                                    
+            setattr(self, 'bn_{}'.format(i+1), BatchNormalization(name='bn_{}'.format(i+1)))
+
+
+    def call(self, inputs, training=False):
+        h = inputs  
+        for i in range(len(self.layer_per_block)):
+            h = getattr(self, 'dense_{}'.format(i+1))(h)
+            h = getattr(self, 'bn_{}'.format(i+1))(h, training=training)
+        return h + inputs
+
